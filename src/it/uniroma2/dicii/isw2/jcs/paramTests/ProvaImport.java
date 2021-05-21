268955e6246393a7724d2d373c5330c867f13d59
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2021-05-21 12:02:51 +0200

    SC #3

--- src/it/uniroma2/dicii/isw2/jcs/paramTests/ProvaImport.java
+++ src/it/uniroma2/dicii/isw2/jcs/paramTests/ProvaImport.java
@@ -2,8 +2,12 @@ package it.uniroma2.dicii.isw2.jcs.paramTests;
 
 import org.apache.jcs.JCS;
 import org.apache.commons.logging.Log;
+import java.lang.System;
 
 public class ProvaImport {
 	//Prova travis
 	
+	public void stampa() {
+		System.out.println("Hello,World!");
+	}
 }
